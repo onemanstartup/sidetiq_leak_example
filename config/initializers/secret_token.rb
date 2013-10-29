01f89b06493227bddf19cdd9280eeb3e7c4f10ee
Plehanov Dmitriy <onemanstartup@gmail.com>
2013-10-29 13:02:09 +0400

    sidetiq leak

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sidde::Application.config.secret_token = '572a2d95848d74658d7ddc87456fb866d4f90e9d7c9a7b6f31b6585540d11038d3f424db32dc0ebf6d06ecfe57e93cbb3ee8b17c9fa955f0cd877baf5db8eb76'
